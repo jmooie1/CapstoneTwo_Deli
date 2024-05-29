fc338f8d368371ac18cbbf779c9fcf263f1790ba
Jason <jmdestiny13@gmail.com>
2024-05-29 15:14:24 -0400

    Product class comments done

--- src/main/java/com/ps/Product.java
+++ src/main/java/com/ps/Product.java
@@ -1,19 +1,23 @@
 package com.ps;
 
 public class Product {
+    // Fields that are being used to store the name and the price of the product.
     private String name;
     private double price;
 
+    // Constructor to initalize the Product object with the name and price.
     public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
+        this.name = name; // Initializes the name of the product
+        this.price = price; // Initializes the price of the product
     }
 
+    // Getter method to retrieve the name of the product
     public String getName() {
-        return name;
+        return name; // Returns the name of the product
     }
 
+    // Getter method to retrieve the price of the product
     public double getPrice() {
-        return price;
+        return price; // Returns the price of the product
     }
 }
