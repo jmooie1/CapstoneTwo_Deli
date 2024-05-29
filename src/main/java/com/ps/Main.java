d7c80711745e51a6b5e9a1c520840c9831f6bb4e
Jason <jmdestiny13@gmail.com>
2024-05-29 15:36:27 -0400

    Comments done on main

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -4,7 +4,10 @@ package com.ps;
 public class Main {
     public static void main(String[] args) {
         // Math adds up to the amount needed for each sandwich.
+        // This creates an instance of home screen to display the main menu and to handle user input
         HomeScreen homeScreen = new HomeScreen();
+
+        // Calls the showOptions method to jump start the application and displays the main menu.
         homeScreen.showOptions();
 
     }
